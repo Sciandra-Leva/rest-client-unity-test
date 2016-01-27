abd831f2b8e53c09d2e14bd9c4dd5dfb1d8462db
Lorenzo Sciandra <sciandra@leva.io>
2016-01-27 16:54:28 +0100

    Changing plugging to embed JSON, the other one was a mess.

--- REST client/Assets/NetworkRESTScript.cs
+++ REST client/Assets/NetworkRESTScript.cs
@@ -2,6 +2,15 @@
 using System.Collections;
 using System.Net;
 using System.IO;
+using System.Text;
+using SimpleJSON;
+
+//public class MyUser
+//{
+//	public int level;
+//	public float timeElapsed;
+//	public string playerName;
+//}
 
 public class NetworkRESTScript : MonoBehaviour {
 
@@ -9,6 +18,7 @@ public class NetworkRESTScript : MonoBehaviour {
 
 	void Start() {
 		StartCoroutine(GETUser(2));
+		StartCoroutine(POSTUser());
 	}
 
 	// Use this to GET single user data
@@ -16,13 +26,13 @@ public class NetworkRESTScript : MonoBehaviour {
 		WWW userData = new WWW (baseURL + "/api/v1/users/" + userID.ToString());
 		yield return userData;
 		string userDataString = userData.text;
-		JSONObject jsonrepOfPatient = new JSONObject (userData.text);
+		var jsonrepOfPatient = JSON.Parse(userData.text);
 		// just testing what happens in JSON usage
-		print ("The packet is composed of " + jsonrepOfPatient.list.Count);
-		JSONObject j = (JSONObject)jsonrepOfPatient.list [0];
-		print ("The subpacket is composed of " + j.list.Count);
-		JSONObject obj = j["name"];
-		print ("The subpacket name is " + obj.str);
+//		print ("The packet is composed of " + jsonrepOfPatient.list.Count);
+//		var j = (JSONObject)jsonrepOfPatient.list [0];
+//		print ("The subpacket is composed of " + j.list.Count);
+//		var obj = j["name"];
+		print ("The subpacket name is " + jsonrepOfPatient["user"]["name"].Value);
 		print (userDataString);
 	}
 
@@ -39,7 +49,7 @@ public class NetworkRESTScript : MonoBehaviour {
 		WWW patientData = new WWW (baseURL + "/api/v1/patients/" + patientID.ToString());
 		yield return patientData;
 		string patientDataString = patientData.text;
-		JSONObject jsonrepOfPatient = new JSONObject (patientData.text);
+//		JSONObject jsonrepOfPatient = new JSONObject (patientData.text);
 		print (patientDataString);
 	}
 
@@ -51,47 +61,47 @@ public class NetworkRESTScript : MonoBehaviour {
 		print (patientListDataString);
 	}
 
-	// Use this to GET the patients list
-//	IEnumerator POSTuser () {
-//
-//		// Create a form object for sending high score data to the server
-//		WWWForm form = new WWWForm();
-//		// Assuming the perl script manages high scores for different games
-//		form.AddField( "game", "MyGameName" );
-//		// The name of the player submitting the scores
-//		form.AddField( "playerName", playName );
-//		// The score
-//		form.AddField( "score", score );
-//
-//		// Create a download object
-//		WWW download = new WWW( highscore_url, form );
-//
-//		// Wait until the download is done
-//		yield return download;
-//
-//		if(!string.IsNullOrEmpty(download.error)) {
-//			print( "Error downloading: " + download.error );
-//		} else {
-//			// show the highscores
-//			Debug.Log(download.text);
-//		}
-//	}
+	// Use this to POST a new user
+	IEnumerator POSTUser () {
+		// first thing for a POST is to do a form
+		WWWForm form = new WWWForm();
 
-
-//	public static WWW RunServerExtension (string appId, string appKey, string endpoint, string kii_access_token, string msg) 
-//	{ 
-//		WWWForm form = new WWWForm(); 
 //		Hashtable headers = form.headers; 
-//		headers["Content-Type"] = "application/json"; 
-//		headers["x-kii-appid"] = appId; 
-//		headers["x-kii-appkey"] = appKey; 
+		form.headers["Content-Type"] = "application/json"; 
+
+//		MyUser myObject = new MyUser();
+//		myObject.level = 1;
+//		myObject.timeElapsed = 47.5f;
+//		myObject.playerName = "Dr Charles Francis";
+
 //		if(kii_access_token != null) 
 //			headers["Authorization"] = "Bearer " + kii_access_token; 
+
+		string json = "test";
+//		string json = JsonUtility.ToJson(myObject);
+
+		Debug.Log("Formatted JSON = " + json);
+
+//		form.AddField( "user", json.ToString());
+
 //		Hashtable data = new Hashtable(); 
-//		data["message"] = msg; 
+//		data["message"] = "Just doing a test"; 
 //		string json = JSON.JsonEncode(data); 
+//
 //		Debug.Log("Sending: " + json); 
-//		byte[] bytes = Encoding.UTF8.GetBytes(json); 
-//		return new WWW("https://api.kii.com/api/apps/" + appId + "/server-code/versions/current/" + endpoint, bytes, headers); 
-//	}
+//
+		byte[] bytes = Encoding.UTF8.GetBytes(json); 
+//		WWW userListData = new WWW (baseURL + "/api/v1/users", form);
+
+		WWW userListData = new WWW (baseURL + "/api/v1/users", bytes, form.headers);
+		yield return userListData;
+
+		if(!string.IsNullOrEmpty(userListData.error)) {
+			print( "Error downloading: " + userListData.error );
+		} else {
+			// show the highscores
+			Debug.Log(userListData.text);
+			string userListDataString = userListData.text;
+		}
+	}
 }
