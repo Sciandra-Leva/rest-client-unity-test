f113eb9b86153ddf6ef8deb74855642ba833e551
Lorenzo Sciandra <sciandra@leva.io>
2016-01-27 12:21:09 +0100

    A basic get on a single user is working.

---
+++ REST client/Assets/NetworkRESTScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkRESTScript : MonoBehaviour {
+
+	string baseURL = "0.0.0.0:3000"; 
+
+	// Use this for initialization
+	IEnumerator Start () {
+		WWW userData = new WWW (baseURL + "/api/v1/users/3");
+		yield return userData;
+		string userDataString = userData.text;
+		print (userDataString);
+	}
+
+	// Use this for downloading the users list
+	IEnumerator GETUsersList () {
+		WWW userData = new WWW (baseURL + "/api/v1/users");
+		yield return userData;
+		string userDataString = userData.text;
+		print (userDataString);
+	}
+}
