f638af9d28dba1d8a195aa26dadc9a520be8f730
Lorenzo Sciandra <sciandra@leva.io>
2016-01-27 12:33:05 +0100

    Now also GET the list of users.

--- REST client/Assets/NetworkRESTScript.cs
+++ REST client/Assets/NetworkRESTScript.cs
@@ -5,19 +5,19 @@ public class NetworkRESTScript : MonoBehaviour {
 
 	string baseURL = "0.0.0.0:3000"; 
 
-	// Use this for initialization
-	IEnumerator Start () {
+	// Use this to GET single user data
+	IEnumerator GETUser () {
 		WWW userData = new WWW (baseURL + "/api/v1/users/3");
 		yield return userData;
 		string userDataString = userData.text;
 		print (userDataString);
 	}
 
-	// Use this for downloading the users list
+	// Use this to GET the users list
 	IEnumerator GETUsersList () {
-		WWW userData = new WWW (baseURL + "/api/v1/users");
-		yield return userData;
-		string userDataString = userData.text;
-		print (userDataString);
+		WWW userListData = new WWW (baseURL + "/api/v1/users");
+		yield return userListData;
+		string userListDataString = userListData.text;
+		print (userListDataString);
 	}
 }
